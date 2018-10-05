5561fc3fb28e56a8226805693205921cac183509
Todd Langford-Archer <toddlangford-archer@Todds-MacBook-Pro.local>
2018-10-05 15:20:53 +0100

    chapter 21 complete

--- lib/van.rb
+++ lib/van.rb
@@ -11,8 +11,16 @@ class Van
     d_s.rack.each { |bike| @van_bikes << d_s.rack.delete(bike) if !(bike.working?) }
   end
 
-  def deposit(garage)
+  def deposit_bikes_to_garage(garage)
     @van_bikes.each { |bike| garage.bikes << @van_bikes.delete(bike) }
+  end
+
+  def collect_bikes_from_garage(garage)
+    garage.bikes.each { |bike| @van_bikes << garage.bikes.delete(bike) }
+  end
+
+  def deposit_bikes_to_ds(docking_station)
+    @van_bikes.each { |bike| docking_station.bikes << @van_bikes.delete(bike) }
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -24,11 +24,34 @@ describe Van do
       bike = double :bike, working?: false
       van.van_bikes << bike
       garage = double :garage, bikes: []
-      van.deposit(garage)
+      van.deposit_bikes_to_garage(garage)
       expect(van.van_bikes).to eq []
 
     end
+  end
 
+  describe '#collect_bikes_from_garage' do
+
+    it 'loads van with working bikes from garage' do
+      van = Van.new
+      bike = double :bike, working?: true
+      garage = double :garage, bikes: [bike]
+      van.collect_bikes_from_garage(garage)
+      expect(van.van_bikes).to eq [bike]
+    end
+
+  end
+
+  describe '#deposit_bikes_to_ds' do
+    it 'unloads the bikes from the van to the docking station' do
+      van = Van.new
+      bike = double :bike, working?: true
+      van.van_bikes << bike
+      docking_station = double :docking_station, bikes: []
+      van.deposit_bikes_to_ds(docking_station)
+      expect(van.van_bikes).to eq []
+      expect(docking_station.bikes).to eq [bike]
+    end
   end
 
 end
